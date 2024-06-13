9626cb48b57662e26e399d00489c993ae450a89b
Sai-Deepak-ale <128961038+saideepak-1563@users.noreply.github.com>
2024-06-13 07:03:02 +0000

    dataTypes

---
+++ javascript/02_dataTypes.js
@@ -0,0 +1,19 @@
+// "use strict";  //It treats all JS code as newer version of node
+
+let name="deepak"
+let age = 22
+let loggedIn = false
+let money = ''
+let state;
+
+// number => 2 to power of 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => assigned but value not defined
+// symbol => unique
+
+
+// object // null is object
+
+console.log(typeof money)
